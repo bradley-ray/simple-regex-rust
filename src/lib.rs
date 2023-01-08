b80ac013d33cda3fd6ee4395887fbd842dc1862c
Bradley Ray <bradleyr320@gmail.com>
2023-01-08 11:57:55 -0600

    git commit -m "setting up everything"

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,41 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct RegExpr {}
+
+// starting with just simple stuff currently
+// when this works, then will handle other
+// stuff such as []  & ()
+pub enum Token {
+    Any,
+    Char(char),
+    Quantifier(char),
+}
+
+fn lexer(src: &str) -> Vec<Token> {
+    let tokens = Vec::with_capacity(64);
+    for (i, chr) in src.chars().enumerate() {
+        todo!();
+    }
+
+    tokens
+}
+
+pub fn compile(src: &str) -> RegExpr {
+    let tokens = lexer(src);
+    for (i, token) in tokens.iter().enumerate() {
+        match token {
+            _ => todo!(),
+        }
+    }
+    RegExpr{}
+}
+
+// start with just match for now
+impl RegExpr {
+    pub fn match_expr(&self,  src: &str) ->  Option<String> {
+        None
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
